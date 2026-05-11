db40bcb08b9323b36da3ff4eb584313b72995f42
Fran Rodriguez <fran02_1996@hotmail.com>
2026-05-11 18:52:57 -0300

    backend ready for deploy

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -1,26 +1,35 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-//Conexion a la base de datos:
-export const baseDatos= mysql.createPool ({
-    password: process.env.BASEDATOS_KEY,
-    host:"localhost",
-    user: "root",
-    database: process.env.DATA_BASE_MENU,
-    
-})
+// Conexion a la base de datos
+export const baseDatos = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 export async function testDataBaseConnection() {
-    let connection;
-    try {
-        connection= await baseDatos.getConnection ()
-    
-    } catch (errorConnection){
-        console.error ("Error al tratar de testear la conexion a la base de datos", errorConnection)
-    }
-     finally {
-        connection?.release () //Para limpiar consulta en basedatos y no ocupar espacio innecesario
-    }
+  let connection;
+
+  try {
+    connection = await baseDatos.getConnection();
+
+    console.log("Base de datos conectada correctamente");
+
+  } catch (errorConnection) {
+
+    console.error(
+      "Error al tratar de conectar la base de datos",
+      errorConnection
+    );
+
+  } finally {
+    connection?.release();
+  }
 }
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
 import express from "express";
 import cors from "cors";
 import { testDataBaseConnection } from "./data/connection";
-import chatRouter from "./routes/chat.routes"
+import chatRouter from "./routes/chat.routes";
 
-const app= express ();
-const PORT= 3000;
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
 
-// DEBUG
-app.use((req, res, next) => {
-    next();
-});
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://menusafe.online"
+  ]
+}));
 
 app.use("/api/chat", chatRouter);
 
-app.get("/api/health", (req, res)=>{
-    res.json({ok:true})
-})
+app.get("/api/health", (req, res) => {
+  res.json({ ok: true });
+});
 
-app.listen(PORT, async ()=>{
-   try { 
-     console.log(`Servidor corriendo en puerto ${PORT}`);
-    testDataBaseConnection();
-   } catch (error){
-    console.error ("No se pudo conectar a la base de datos, apagando el servidor..")
-    process.exit (1); //Apaga el proceso de node y termina en error
-   }
-})
+app.listen(PORT, async () => {
+  try {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+
+    await testDataBaseConnection();
+
+  } catch (error) {
+    console.error("No se pudo conectar a la base de datos");
+    process.exit(1);
+  }
+});
